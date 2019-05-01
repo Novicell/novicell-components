55cb00467aa82e0c5354e19b57b14dcbe4c1ded0
henrikschytze <henrikschytze@gmail.com>
2019-05-01 15:06:55 +0200

    trying with dynamic npm install per key in json

--- copyDependencies.js
+++ copyDependencies.js
@@ -1,8 +1,15 @@
+let exec = require('child_process').exec;
 const fs = require("fs");
 let componentDependencies = require("./component_dependencies.json");
 let frontendPackageJson = require("../package.json");
-frontendPackageJson.dependencies = componentDependencies.dependencies;
-fs.writeFileSync("../package.json", JSON.stringify(frontendPackageJson), (err)=>{
-    console.log(err)
-})
+let keys = Object.keys(componentDependencies.dependencies);
+let child;
+console.log(keys)
+keys.forEach((key)=>{
+    child = exec(`npm install ${key}`).stderr.pipe(process.stderr);
+});
+// frontendPackageJson.dependencies = componentDependencies.dependencies;
+// fs.writeFileSync("../package.json", JSON.stringify(frontendPackageJson), (err)=>{
+//     console.log(err)
+// })
 

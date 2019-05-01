6911c1ab2f9a3c5bc4b6b691944a552bb07e94b6
henrikschytze <henrikschytze@gmail.com>
2019-05-01 15:27:33 +0200

    reverted node file

--- copyDependencies.js
+++ copyDependencies.js
@@ -1,10 +1,14 @@
 let exec = require('child_process').exec;
+const fs = require("fs");
 let componentDependencies = require("./component_dependencies.json");
+let frontendPackageJson = require("../package.json");
 let keys = Object.keys(componentDependencies.dependencies);
 let child;
-console.log("Installing the following dependencies, please wait...")
 console.log(keys)
 keys.forEach((key)=>{
     child = exec(`npm install ${key}`);
 });
-
+// frontendPackageJson.dependencies = componentDependencies.dependencies;
+// fs.writeFileSync("../package.json", JSON.stringify(frontendPackageJson), (err)=>{
+//     console.log(err)
+// })

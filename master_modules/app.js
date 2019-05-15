ad1d15fc3bc442d404cc37129d711e89f818c385
henrikschytze <henrikschytze@gmail.com>
2019-05-15 14:52:40 +0200

    Added remaining js from juozas

--- master_modules/app.js
+++ master_modules/app.js
@@ -6,11 +6,11 @@ import '../02-molecules/navigation/nav-trigger/nav-trigger';
 import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
 import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
 import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
-// import '../03-organisms/topbar/topbar-related/topbar-related';
+import '../03-organisms/topbar/topbar-related/topbar-related';
 import '../03-organisms/pageheaders/pageheader-video/pageheader-video--youtube';
 
-// import NovicellLazyLoad from 'novicell-lazyload';
-// import debounce from 'lodash/debounce';
+import NovicellLazyLoad from 'novicell-lazyload';
+import debounce from 'lodash/debounce';
 
-// document.addEventListener('lazybeforeunveil', NovicellLazyLoad.lazyLoad, true);
-// window.addEventListener('resize', debounce(NovicellLazyLoad.checkImages), 100, false);
+document.addEventListener('lazybeforeunveil', NovicellLazyLoad.lazyLoad, true);
+window.addEventListener('resize', debounce(NovicellLazyLoad.checkImages), 100, false);

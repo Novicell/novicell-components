e3e87071723062ec7a9c2c7aa70a06b097e5e363
henrikschytze <henrikschytze@gmail.com>
2019-05-15 08:51:43 +0200

    added the master files to the new master_modules folder

---
+++ master_modules/app.js
@@ -0,0 +1,16 @@
+// // This is the global JS file that will be accessible from any component
+// import 'novicell-lazyload';
+
+
+// import '../02-molecules/navigation/nav-trigger/nav-trigger';
+// import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
+// import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
+import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
+// import '../03-organisms/topbar/topbar-related/topbar-related'; // <-this was messing with the build process
+import '../03-organisms/pageheaders/pageheader-video/pageheader-video--youtube';
+
+// import NovicellLazyLoad from 'novicell-lazyload';
+// import debounce from 'lodash/debounce';
+
+// document.addEventListener('lazybeforeunveil', NovicellLazyLoad.lazyLoad, true);
+// window.addEventListener('resize', debounce(NovicellLazyLoad.checkImages), 100, false);

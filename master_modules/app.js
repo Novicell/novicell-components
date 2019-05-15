593da5201abd5f3faba5fc040fe62c5c59ee2e83
henrikschytze <henrikschytze@gmail.com>
2019-05-15 13:34:10 +0200

    trying without topbar-related.js

--- master_modules/app.js
+++ master_modules/app.js
@@ -6,7 +6,7 @@ import '../02-molecules/navigation/nav-trigger/nav-trigger';
 import '../02-molecules/navigation/nav-main-list-priority/nav-main-list-priority';
 import '../03-organisms/pageheaders/pageheader-slider/pageheader-slider';
 import '../03-organisms/pageheaders/pageheader-dots/pageheader-dots';
-import '../03-organisms/topbar/topbar-related/topbar-related';
+// import '../03-organisms/topbar/topbar-related/topbar-related';
 import '../03-organisms/pageheaders/pageheader-video/pageheader-video--youtube';
 
 // import NovicellLazyLoad from 'novicell-lazyload';

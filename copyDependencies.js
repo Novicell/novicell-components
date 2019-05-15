3e7c59414318a69bb2c3c328a990becd2ffff318
henrikschytze <henrikschytze@gmail.com>
2019-05-15 13:50:53 +0200

    trying to npm i with npm module instead

--- copyDependencies.js
+++ copyDependencies.js
@@ -1,8 +1,42 @@
 let exec = require('child_process').exec;
 let componentDependencies = require("./component_dependencies.json");
 let keys = Object.keys(componentDependencies.dependencies);
+let npm = require('npm');
 console.log(keys)
-// Run npm install for each key (key is dependency name, I.E "novicell-sprite-scroller")
-keys.forEach((key)=>{
-    child = exec(`npm install ${key}`).stderr.pipe(process.stderr);
+
+keys.forEach((key) => {    
+    npm.load(function (err) {
+        // handle errors
+        if (err) {
+            console.log(err)
+        };
+        // install module ffi
+        npm.commands.install([`${key}`], function (er, data) {
+            if(er){
+                console.log(er)
+            }
+            console.log(data)
+            // log errors or data
+        });
+
+        npm.on('log', function (message) {
+            // log installation progress
+            console.log(message);
+        });
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+// Run npm install for each key (key is dependency name, I.E "novicell-sprite-scroller")
+// keys.forEach((key)=>{
+//     child = exec(`npm install ${key}`).stderr.pipe(process.stderr);
+// });

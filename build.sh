f3d3ccda82f7ea13a9da290acc42a3c480d2f0ba
henrikschytze <henrikschytze@gmail.com>
2019-05-21 12:17:32 +0200

    missing dot

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # Function for cloning the repo and copying it into the src
 clone_components()
 {
-    mv ./*/ /tmp_src/
+    mv ./*/ ./tmp_src/
 
     git clone https://github.com/Henrikschytze/Continuous_integration_frontend.git .
     echo "Copying asset folders from tmp_src to ./assets/"

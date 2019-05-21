c49089e5d606292285ecee1b02c64497092a9d90
henrikschytze <henrikschytze@gmail.com>
2019-05-21 13:37:19 +0200

    Improved echos and added additional comments for future reference

--- build.sh
+++ build.sh
@@ -4,20 +4,21 @@ shopt -s extglob
 # Function for cloning the repo and copying it into the src
 clone_components()
 {
+    echo "Moving all the relevant folders into a folder called tmp_src"
     mv -v ./_base/ ./_partials/ ./00-examples/ ./01-atoms/ ./02-molecules/ ./03-organisms/ ./04-pages/ ./assets/ ./master_modules ./tmp_src/ 
-
     # Cloning into a tmp folder as it is not allowed to clone into current folder if it is not empty
-
     git clone https://github.com/Henrikschytze/Continuous_integration_frontend.git tmp_frontend
-
+    echo "The frontend was cloned into a temporary folder called tmp_frontned"
+    echo "Copying the contents of /tmp_frontend/ to ./"
     cp -a -rf ./tmp_frontend/* ./
-
     echo "Copying asset folders from tmp_src to ./assets/"
     cp -a -rf ./tmp_src/assets/* ./assets/
     # Remove the temp assets so they're not copied alongside the other contents of the components dir
-    rm -rf ./tmp_src/assets/
-    rm -rf -v ./tmp_frontend/
+    echo "Removing temporary assets folder and temporary frontend repo folder"    
+    rm -rf ./tmp_src/assets/    
+    rm -rf ./tmp_frontend/
     # Copy all the remaining contents of tmp_src to ./src/
+    echo "Copying all the remaining files from /tmp_src to ./src"    
     cp -a -rf ./tmp_src/* ./src/
     # Copying master.css and app.js from components to main dir
     echo "Overriding inner contents from master_modules/app.js and master.css to src/Modules"
